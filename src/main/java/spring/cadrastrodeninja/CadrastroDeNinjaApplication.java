92cb7e26ee03f81bbfe1e014df28a1769a735458
Jeannfreitas <freitasjean344@gmail.com>
2025-07-26 00:39:24 -0300

    first commit

---
+++ src/main/java/spring/cadrastrodeninja/CadrastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package spring.cadrastrodeninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadrastroDeNinjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadrastroDeNinjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/cadrastrodeninja/CadrastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cadrastrodeninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadrastroDeNinjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
